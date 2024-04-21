bc5b5e7df82dd3450f6ded1f19766b29929440d8
El chafar <chafargetanibal@gmail.com>
2024-04-21 17:28:22 -0400

    correccion ultima subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import * as AOS from 'aos';
+export const CC_PROJECT_INITIALS = 'f-template';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'nerdsweb';
+
+  ngOnInit(): void {
+    AOS.init();
+    window.addEventListener('load', AOS.refresh)
+  }
+
 }
